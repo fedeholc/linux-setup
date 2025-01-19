22f93b600fad9503cb641bea7754c7255fe410e0
Federico Holc <federicoholc@gmail.com>
2025-01-19 08:53:06 -0300

    migrate installation commands from installs.md to installs-con-apt.sh

---
+++ installs-con-apt.sh
@@ -0,0 +1,15 @@
+sudo apt install dropbox -y
+sudo apt install gimp -y
+sudo apt install git -y
+sudo apt install gpa -y
+sudo apt install kazam -y
+sudo apt install kupfer -y
+sudo apt install pdfarranger -y
+sudo apt install samba -y
+sudo apt install smplayer -y
+sudo apt install unrar -y
+sudo apt install vlc -y
+
+# para los shortcuts
+sudo apt install wmctrl -y
+sudo apt install xdotool -y
--- installs.md
+++ installs.md
@@ -1,17 +1,3 @@
-sudo apt install dropbox -y
-sudo apt install gimp -y
-sudo apt install git -y
-sudo apt install gpa -y
-sudo apt install kazam -y
-sudo apt install kupfer -y
-sudo apt install pdfarranger -y
-sudo apt install samba -y
-sudo apt install smplayer -y
-sudo apt install unrar -y
-sudo apt install vlc -y
-
-sudo apt install wmctrl -y
-sudo apt install xdotool -y
 
 apps:
 
